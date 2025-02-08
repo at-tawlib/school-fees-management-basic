706e0160953dbd45809eb831fbf0072be5f6fb9e
Abdul-Fatahu Hardi <azindodev@gmail.com>
2025-02-08 09:30:17 +0000

    Update forge config

--- forge.config.js
+++ forge.config.js
@@ -1,3 +1,6 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
 module.exports = {
   packagerConfig: {
     asar: true,
@@ -7,38 +10,51 @@ module.exports = {
     {
       name: "@electron-forge/maker-squirrel",
       config: {
-        name: "School Fees Management",
+        name: "school-fees-management",
         overwrite: true,
       },
     },
     {
       name: "@electron-forge/maker-zip",
-      platforms: ["darwin", "linux"],
-      config: {
-        name: "School Fees Management",
-        overwrite: true,
-      },
+      platforms: ["darwin"],
     },
     {
       name: "@electron-forge/maker-deb",
       config: {
-        name: "School Fees Management",
+        name: "school-fees-management",
         overwrite: true,
       },
     },
     {
       name: "@electron-forge/maker-rpm",
       config: {
-        name: "School Fees Management",
+        name: "school-fees-management",
         overwrite: true,
       },
     },
     {
       name: "@electron-forge/maker-dmg",
       config: {
-        name: "School Fees Management",
+        name: "school-fees-management",
         overwrite: true,
       },
     },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-auto-unpack-natives",
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
   ],
 };
