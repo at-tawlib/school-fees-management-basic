ef23e34f5c429037fa07b9ba80afe30baea596d0
Abdul-Fatahu Hardi <azindodev@gmail.com>
2025-02-08 02:51:30 +0000

    Update configs

--- forge.config.js
+++ forge.config.js
@@ -1,30 +1,44 @@
 module.exports = {
-    packagerConfig: {},
-    rebuildConfig: {},
-    makers: [
-      {
-        name: "@electron-forge/maker-squirrel",
-        config: {},
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "School Fees Management",
+        overwrite: true,
       },
-      {
-        name: "@electron-forge/maker-zip",
-        platforms: ["darwin", "linux"],
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin", "linux"],
+      config: {
+        name: "School Fees Management",
+        overwrite: true,
       },
-      {
-        name: "@electron-forge/maker-deb",
-        config: {},
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {
+        name: "School Fees Management",
+        overwrite: true,
       },
-      {
-        name: "@electron-forge/maker-rpm",
-        config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {
+        name: "School Fees Management",
+        overwrite: true,
       },
-      {
-        name: "@electron-forge/maker-dmg",
-        config: {
-          name: "School Fees Management",
-          overwrite: true,
-        },
+    },
+    {
+      name: "@electron-forge/maker-dmg",
+      config: {
+        name: "School Fees Management",
+        overwrite: true,
       },
-    ],
-  };
-  
+    },
+  ],
+};
