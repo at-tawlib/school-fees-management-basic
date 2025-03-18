8aff9d78941d0092e0a19997e723219d9100af35
Abdul-Fatahu Hardi <azindodev@gmail.com>
2025-03-18 16:33:22 +0000

    Improve UI of toast

--- src/js/utils/toast.js
+++ src/js/utils/toast.js
@@ -1,28 +1,20 @@
 // Function to show a toast notification
 export function showToast(message, type = "success") {
-  const toastText = document.createElement("p");
-  toastText.className = "toast-text";
-  toastText.textContent = message;
+  const toastContainer = document.getElementById("toastContainer");
+  const toastText = document.getElementById("toastText");
+  const closeButton = document.getElementById("toastClose");
 
-  // Create close button
-  const closeButton = document.createElement("button");
-  closeButton.className = "toast-close-button";
-  closeButton.innerHTML = "&times;";
+  toastContainer.style.display = "";
+  toastText.textContent = message;
   closeButton.onclick = () => {
-    toastText.remove();
+    toastContainer.style.display = "none";
   };
 
-  // Append close button to toast
-  toastText.appendChild(closeButton);
-
-  // Append toast to container
-  const container = document.getElementById("toastContainer");
-  container.classList.remove("toast-success", "toast-error");
-  container.appendChild(toastText);
-  container.classList.add(`toast-${type}`);
+  toastContainer.classList.remove("toast-success", "toast-error");
+  toastContainer.classList.add(`toast-${type}`);
 
   // Remove toast after 3 seconds
   setTimeout(() => {
-    toastText.remove();
+    toastContainer.style.display = "none";
   }, 10000);
 }
